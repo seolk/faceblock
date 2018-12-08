8889ec28792e001b361353959ab0615221ad7394
Nikki Johnson <nikkijohnson@Nikkis-MacBook-Air.local>
2018-12-08 10:53:55 -0700

    finished post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,43 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
+    @posts = current_user.posts.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
   end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
