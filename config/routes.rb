20a9742390f5792303e01d548c9b3e751c8c1fb8
Seol Kim <oksusu123@gmail.com>
2018-12-08 09:48:33 -0700

    added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/new'
+  get 'comments/show'
+  get 'comments/edit'
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/show'
+  get 'posts/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
